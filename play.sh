44ad45d4d47cafcbcb1a47b1902cdbea16f8bc3f
Zachary S <pizzapants111@gmail.com>
2020-05-24 03:53:26 -0500

    .

--- play.sh
+++ play.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [[ "$1" = "--flute" ]]; then
-	./midi.py "$2" | ./midi_flute
-elif [[ "$1" = "--saw" ]]; then
-	./midi.py "$2" | ./midi_saw
-elif [[ "$1" = "--triangle" ]]; then
-	./midi.py "$2" | ./midi_triangle
-elif [[ "$1" = "--square" ]]; then
-	./midi.py "$2" | ./midi_square
-elif [[ "$1" = "--test" ]]; then
-	./midi.py "$2" | ./midi_test
-elif [[ "$1" = "--violin" ]]; then
-	./midi.py "$2" | ./midi_violin
-else
-	./midi.py "$1" | ./midi_violin
-fi
+#if [[ "$1" = "--flute" ]]; then
+#	./midi.py "$2" | ./midi_flute
+#elif [[ "$1" = "--saw" ]]; then
+#	./midi.py "$2" | ./midi_saw
+#elif [[ "$1" = "--triangle" ]]; then
+#	./midi.py "$2" | ./midi_triangle
+#elif [[ "$1" = "--square" ]]; then
+#	./midi.py "$2" | ./midi_square
+#elif [[ "$1" = "--test" ]]; then
+#	./midi.py "$2" | ./midi_test
+#elif [[ "$1" = "--violin" ]]; then
+#	./midi.py "$2" | ./midi_violin
+#else
+	./midi.py "$1" | ./midi
+#fi
